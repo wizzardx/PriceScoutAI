f5883eaff5f671efe5da8b8ed5d163aadfc99a69
david <wizzardx@gmail.com>
2025-01-12 10:55:17 +0200

    Add test for description parsing

--- tests/test_price_scouter.py
+++ tests/test_price_scouter.py
@@ -1,2 +1,16 @@
+from pricescoutai import ProductParser
+
 def test_example():
     assert False, "This is a failing test to get started!"
+
+def test_parse_description():
+    description = "Nylon Braided 3-in-1 Multi-Charging Cable"
+    expected = {
+        'type': '3-in-1 Multi-Charging Cable',
+        'features': ['Nylon Braided', 'USB Power Mode', '≤36V Operating Voltage'],
+        'compatible_devices': ['iPhone', 'Android']
+    }
+    parser = ProductParser(description)
+    result = parser.parse_description()
+    
+    assert result == expected
